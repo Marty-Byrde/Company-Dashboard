9ccf65b6187776c7662b2d9405f86b3e9bdc84f0
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-01-03 12:19:14 +0100

    feat: added CloseOnNavigation component
    This component closes the search-dialog when the user has navigated.

---
+++ app/(components)/Shared/Filtering/DialogSubcomponents/CloseOnNavigation.tsx
@@ -0,0 +1,15 @@
+import { usePathname, useSearchParams } from "next/navigation"
+import { useContext, useEffect } from "react"
+import { SearchDialogContext } from "@/components/Shared/Filtering/SearchDialog"
+
+export default function CloseOnNavigation() {
+  const { close, autocomplete } = useContext(SearchDialogContext)
+  let pathname = usePathname()
+  let searchParams = useSearchParams()
+
+  useEffect(() => {
+    close()
+  }, [pathname, searchParams, close, autocomplete])
+
+  return null
+}
