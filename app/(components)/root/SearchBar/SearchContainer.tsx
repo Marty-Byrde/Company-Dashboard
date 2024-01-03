0e99e1c1d83289a9cff4ade499c0cdfe7be2c453
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-01-03 12:48:56 +0100

    feat: added SearchContainer component
    This component may be placed in the layout of a page to display a container on desktop-screens that shows a short-title for the page and a search-bar.

---
+++ app/(components)/root/SearchBar/SearchContainer.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { GlobalColorSettings } from '@/lib/root/ColorSettings'
+import Search, { SearchItem } from '@/components/Shared/Filtering/Search'
+import { twMerge } from 'tailwind-merge'
+
+const { lightBackground, darkBackground } = GlobalColorSettings
+
+export default function SearchContainer({ title, className, items }: { title?: string; items: SearchItem[]; className?: string }) {
+  return (
+    <div
+      className={twMerge('-mx-4 -mt-4 hidden h-16 items-center justify-between border-b px-4 lg:flex dark:border-b-neutral-600', lightBackground, darkBackground, title ?? 'justify-end')}
+      key={lightBackground + darkBackground}>
+      <span className={twMerge('flex-1 text-lg', className, title ?? 'hidden')}>{title}</span>
+      <div className='flex max-w-md flex-1 items-center gap-4'>
+        <Search items={items} shortCutKey='i' />
+      </div>
+    </div>
+  )
+}
