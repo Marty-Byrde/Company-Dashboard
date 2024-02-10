b066ab44ea2f167192c01fdc09793b6dc59befce
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-02-10 09:44:20 +0100

    docs: added example to useManipulation hook

--- app/(components)/Shared/Manipulation/useManipulation.tsx
+++ app/(components)/Shared/Manipulation/useManipulation.tsx
@@ -7,6 +7,20 @@ import { ManipulationInput } from './ManipulationInput'
  * @param object The useState-object that should be manipulated
  * @param setObject The setState-function that should be used to update the object
  * @returns The container and input components that are used to manipulate the given object
+ * 
+ * @example
+ * ```tsx
+ * const { Container, Input, containerProps } = useManipulation({ object, setObject })
+ * <Container {...containerProps}>
+    <Input label='Custom-Label' path='isCanceled' />
+
+    <Input path='customer.firstName' />
+    <Input path='customer.lastName' />
+    <Input path='customer.lastName' />
+    <Input path='customer.country' />
+   </Container>
+ * 
+ * ```
  */
 export default function useManipulation<T>({ object, setObject }: { object: ReactState<T>['state']; setObject: ReactState<T>['setState'] }) {
   return {
