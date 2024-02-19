866f10bf06b4f84aaab04ff1907a33f7164f53f6
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-02-19 17:22:37 +0100

    feat: created convertContactInformations module
    This lib-module is used to create a customer-object based on the ContactInformations of a order (billing or shipping information).
    
    While it converts the order-informations to a customer object it also parses the given address, by merging the two address fields of an order. It then converted the combined address into a tuple of street and houseNumber. It does so by looking at the address, when it includes a number, then one can assume that its a houseNumber, thus the address is then split into street and houseNumber.

---
+++ lib/orders/ConvertContactInformations.ts
@@ -0,0 +1,79 @@
+import { Customer } from 'hellocash-api/typings/Customer'
+import Order from 'woocommerce-utils/helper/typings/Order'
+
+export default function convertContactInformations({
+  title_formatted: salutation,
+  first_name: firstName,
+  last_name: lastName,
+  country,
+  city,
+  postcode: postCode,
+  address_1,
+  address_2,
+  phone,
+  company,
+  email,
+  state,
+}: Order['shipping'] & { email?: string }): Omit<Customer, 'id' | 'timestamp'> {
+  const { street, houseNumber } = convertAddress({ addess: mergeAddresses(address_1, address_2) })
+
+  return {
+    salutation,
+    firstName,
+    lastName,
+    email: email ?? '',
+    phone,
+    country,
+    city,
+    postCode,
+    street,
+    houseNumber,
+    birthday: null,
+    notes: [],
+    company,
+    uid_number: '',
+  }
+}
+
+/**
+ * Combines the two addresses into one, as each order has to fields for the address (address_1 and address_2)
+ * @param address1 Typically the main address
+ * @param address2 Sometimes the address_2 is used for the housenumber or additional information
+ */
+function mergeAddresses(address1: string, address2: string) {
+  if (address2.trim().length === 0) return address1
+
+  const uniqueAddress2 = address2.replace(address1, '').trim()
+
+  return address1 + ' ' + uniqueAddress2
+}
+
+/**
+ * Converts the address into a street and a house number
+ * @param initialAddress The address that is to be converted
+ * @returns The street and the house number as a tuple
+ */
+function convertAddress({ addess: initialAddress }: { addess: string }) {
+  let street = initialAddress
+  let houseNumber: string = ''
+
+  // if the address contains a number, then probabbly has a housenumber
+  if (
+    street.includes('0') ||
+    street.includes('1') ||
+    street.includes('2') ||
+    street.includes('3') ||
+    street.includes('4') ||
+    street.includes('5') ||
+    street.includes('6') ||
+    street.includes('7') ||
+    street.includes('8') ||
+    street.includes('9')
+  ) {
+    let index = street.search(/\d/)
+    houseNumber = street.slice(index)
+    street = street.slice(0, index)
+  }
+
+  return { street, houseNumber }
+}
