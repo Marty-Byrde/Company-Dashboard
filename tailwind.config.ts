360a90b767d13d5852b20712590736f6a3321344
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-02-19 17:05:58 +0100

    chore: extended container-query sizes
    Added the following container-query options:
    - 2xs: 16rem
    - 3xs: 12rem

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,10 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      containers: {
+        '2xs': '16rem',
+        '3xs': '12rem',
+      },
     },
   },
   plugins: [require('@tailwindcss/container-queries'), require('@tailwindcss/forms')],
