5adb59cf334e783ecbbcb3fdf4d12607d2880930
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-02-20 13:55:49 +0100

    feat: added Warning-Menu component
    This component is rendered when the billing and shipping informations of an order are different. It then shows a warning and two buttons, that allow the user to select which one of the information are to be used.
    
    To ensure that the CustomerProps component is re-rendered when the base-values are changed. For example from billing to shipping or the other way around. The key property of the CustomerProps component was set to be a string of order.id and baseInformation. This way the CustomerProps component gets updated when the baseInformations or oder.id is changed.

--- app/(components)/orders/CustomerRegistrationMenu.tsx
+++ app/(components)/orders/CustomerRegistrationMenu.tsx
@@ -3,14 +3,20 @@ import { useEffect } from 'react'
 import useManipulation from '../Shared/Manipulation/useManipulation'
 import { useSlideOver } from '../Shared/Slideover/SlideoverProvider'
 import { useCustomerSlideOverContext } from './RegistrationProvider'
+import convertContactInformations, { hasDifferentAddresses } from '@/lib/orders/ConvertContactInformations'
+import { twMerge } from 'tailwind-merge'
+import { ExclamationCircleIcon } from '@heroicons/react/24/outline'
 
 export default function CustomerRegistrationMenu() {
   const { close } = useSlideOver()
+  const { order, baseInformation } = useCustomerSlideOverContext()
 
   return (
     <form>
+      <WarningMenu shown={hasDifferentAddresses({ order })} />
+
       <div className='flex flex-1 flex-col gap-4 overflow-y-auto px-4 pb-4'>
-        <CustomerProps />
+        <CustomerProps key={order?.id + baseInformation} />
       </div>
       <div className='mt-4 flex justify-center gap-4 border-t-2 px-4 py-4 dark:border-t-neutral-700'>
         <button className='rounded-md px-4 py-2 ring-1 dark:text-gray-300/90 dark:ring-gray-400/60' onClick={close} type='button'>
@@ -21,6 +27,44 @@ export default function CustomerRegistrationMenu() {
         </button>
       </div>
     </form>
+  )
+}
+
+/**
+ * Displays a warning if the billing and shipping addresses are different. The user can then select which address he wants to use.
+ * @param shown If the warning should be shown
+ */
+function WarningMenu({ shown }: { shown: boolean }) {
+  const { baseInformation, setBaseInformation, order, setCustomer } = useCustomerSlideOverContext()
+
+  return (
+    <div className={twMerge('-mt-4 hidden pt-2', shown && 'mb-4 flex flex-col gap-4')}>
+      <div className='flex items-center justify-center gap-2 font-semibold dark:text-orange-400'>
+        <ExclamationCircleIcon height={28} className='stroke-2' width={28} />
+        <span className='text-xs xs:text-sm md:text-base'>Rechnungsanschrift ungleich Lieferanschrift</span>
+      </div>
+      <div className='flex items-center justify-evenly'>
+        <button
+          type='button'
+          className={twMerge('rounded-md px-4 py-2', baseInformation === 'billing' ? 'dark:bg-blue-500/60' : 'border dark:border-gray-500')}
+          onClick={() => {
+            setBaseInformation('billing')
+            setCustomer(convertContactInformations(order!.billing))
+          }}>
+          Rechnungsanschrift
+        </button>
+        <button
+          type='button'
+          className={twMerge('rounded-md px-4 py-2', baseInformation === 'shipping' ? 'dark:bg-blue-500/60' : 'border dark:border-gray-500')}
+          onClick={() => {
+            setBaseInformation('shipping')
+            setCustomer(convertContactInformations(order!.shipping))
+          }}>
+          Liefersanschrift
+        </button>
+      </div>
+      <hr className='border-dashed border-neutral-400' />
+    </div>
   )
 }
 
