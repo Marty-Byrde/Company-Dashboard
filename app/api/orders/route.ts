b69d820fbc10cfce86580e98dc439eccca0318d5
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-02-22 18:20:50 +0100

    refactor: updated api-endpoint parameters
    Removed the invalid type of the req object. Instead added the params as a separate argument to the endpoint-params.

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -1,9 +1,8 @@
-import { NextApiRequest } from 'next'
 import { NextResponse } from 'next/server'
 import getWoocommerceApi from '@/lib/Shared/Woocommerce'
 
-export async function GET(req: NextApiRequest) {
-  const limit: unknown = req.query?.limit
+export async function GET(req: Request, context: { params: any }) {
+  const limit = context.params?.limit
   const { getOrders } = await getWoocommerceApi()
   const orders = await getOrders(parseInt(limit?.toString() ?? '25'))
 
