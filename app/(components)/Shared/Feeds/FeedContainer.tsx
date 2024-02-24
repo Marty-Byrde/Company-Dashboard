dfaea4fe81fd688df6d69087ecc4c0f572b16a59
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-02-24 18:21:52 +0100

    feat: added FeedContainer component
    This component wraps around future feeds and essentially manages the spacing between its children (feeds).

---
+++ app/(components)/Shared/Feeds/FeedContainer.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export default function FeedContainer({ children, className }: { children?: ReactNode; className?: string }) {
+  return (
+    <>
+      <ul role='list' className={twMerge('space-y-6', className)}>
+        {children}
+      </ul>
+    </>
+  )
+}
