184ad00893c22ee774e8405d796585d11c658c42
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-02-28 11:53:27 +0100

    refactor: updated CustomerBuyHistory component
    Instead of passing a customer object to the component, one now passes down the invoices that are to be displayed. The reason for this is, that this component is therefore more generic and can be used in different scenarios.
    
    To use this component to display the customer history, one simply filters the invoices by their customer to match the customer in question.

--- app/customer-history/[cid]/CustomerBuyHistory.tsx
+++ app/customer-history/[cid]/CustomerBuyHistory.tsx
@@ -1,5 +1,4 @@
 import { Invoice } from 'hellocash-api/typings/Invoice'
-import { Customer } from 'hellocash-api/typings/Customer'
 import Each from '@/lib/Shared/Each'
 import { formatDate, formatDistance } from 'date-fns'
 import { CheckCircleIcon } from '@heroicons/react/24/solid'
@@ -8,51 +7,46 @@ import Feed from '@/components/Shared/Feeds/Feed'
 import MinorFeed from '@/components/Shared/Feeds/MinorFeed'
 
 /**
- * Displays a list of invoices of a given customer and the items that were bought in each invoice.
- * @param customer The customer whoos history shall be displayed
+ * Displays a list of invoices in the form of feeds. Each feed contains the invoice number, the customer who bought the items and the items of the invoice themselves.
+ * @param invoices The invoices that are to be displayed. The invoices should be filtered to only contain by the customer in question.
  * @constructor
  */
-export default async function CustomerBuyHistory({ customer }: { customer: Customer; invoices?: Invoice[] }) {
-  let invoices = await fetch(`${process.env.BACKEND}/invoices?limit=-1`, { next: { revalidate: 3600 * 24, tags: ['invoices'] } })
-    .then((res) => res.json())
-    .then((json) => json as Invoice[])
-  invoices = invoices.filter((i) => i.customer?.id == customer.id)
+export default async function CustomerBuyHistory({ invoices }: { invoices: Invoice[] }) {
+  invoices = invoices.filter((i) => !!i.customer)
 
   return (
-    <>
-      <FeedContainer>
-        <Each
-          items={invoices}
-          render={(invoice, index) => (
-            <>
-              <Feed
-                key={invoice.id}
-                link={`/invoice/${invoice.id}`}
-                icon={<CheckCircleIcon className='h-6 w-6 rounded-full text-indigo-600 dark:bg-neutral-700 dark:text-green-500 dark:ring-2 dark:ring-green-500/30' />}
-                title={'#' + invoice.id.toString()}
-                description={`${customer.firstName} ${customer.lastName} bought the following item${invoice.items.length > 1 && 's'}:`}
-                date={`${formatDistance(invoice.timestamp, new Date())} ago`}
-              />
+    <FeedContainer>
+      <Each
+        items={invoices}
+        render={(invoice, index) => (
+          <>
+            <Feed
+              key={invoice.id}
+              link={`/invoice/${invoice.id}`}
+              icon={<CheckCircleIcon className='h-6 w-6 rounded-full text-indigo-600 dark:bg-neutral-700 dark:text-green-500 dark:ring-2 dark:ring-green-500/30' />}
+              title={'#' + invoice.id.toString()}
+              description={`${invoice!.customer!.firstName} ${invoice!.customer!.lastName} bought the following item${invoice.items.length > 1 ? 's' : ''}:`}
+              date={`${formatDistance(invoice.timestamp, new Date())} ago`}
+            />
 
-              <FeedContainer>
-                <Each
-                  items={invoice.items.filter((i) => !i.name.toLowerCase().includes('versand'))}
-                  render={(item, item_index) => (
-                    <MinorFeed
-                      key={invoice.id + '-' + item.id + '-' + item_index}
-                      description={item.name}
-                      title={`Quantity: ${item.quantity}x`}
-                      single={invoice.items.filter((i) => !i.name.toLowerCase().includes('versand')).length === 1}
-                      last={index === invoices.length - 1}
-                      date={formatDate(invoice.timestamp, 'dd. MMM yyyy')}
-                    />
-                  )}
-                />
-              </FeedContainer>
-            </>
-          )}
-        />
-      </FeedContainer>
-    </>
+            <FeedContainer>
+              <Each
+                items={invoice.items.filter((i) => !i.name.toLowerCase().includes('versand'))}
+                render={(item, item_index) => (
+                  <MinorFeed
+                    key={invoice.id + '-' + item.id + '-' + item_index}
+                    description={item.name}
+                    title={`Quantity: ${item.quantity}x`}
+                    single={invoice.items.filter((i) => !i.name.toLowerCase().includes('versand')).length === 1}
+                    last={index === invoices.length - 1}
+                    date={formatDate(invoice.timestamp, 'dd. MMM yyyy')}
+                  />
+                )}
+              />
+            </FeedContainer>
+          </>
+        )}
+      />
+    </FeedContainer>
   )
 }
