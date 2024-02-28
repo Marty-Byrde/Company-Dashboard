1fa5cacc5571d21f8207282a2749603cecdeaf4e
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-02-28 12:44:03 +0100

    feat: created /articles api-endpoint
    Created a new api-endpoint to return a given amount of articles. By default this endpoint will retrieve and then return all the articles.

---
+++ app/api/articles/route.ts
@@ -0,0 +1,12 @@
+import getHellocashAPI from '@/lib/Shared/HelloCash'
+import { NextResponse } from 'next/server'
+
+export async function GET(req: Request) {
+  const params = new URL(req.url).searchParams
+  const limit = parseInt(params?.get('limit')?.toString() ?? '-1')
+
+  const { getArticles } = getHellocashAPI()
+  const articles = await getArticles(limit)
+
+  return NextResponse.json(articles)
+}
