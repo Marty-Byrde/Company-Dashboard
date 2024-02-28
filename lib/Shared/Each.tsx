5c9510fd1128a738b7af637cd613c3020a9e28ab
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-02-28 12:02:32 +0100

    refactor: optimized Each component
    Instead of returning a Fragment with the rendered items, the component now simply returns the items rendered items without a Fragment.

--- lib/Shared/Each.tsx
+++ lib/Shared/Each.tsx
@@ -1,3 +1,5 @@
-export default function Each<T>({ items, render }: { items?: Array<T>; render(item: T, index: number): React.ReactNode }) {
-  return <>{items?.map((item, index) => render(item, index))}</>
+import { ReactNode } from 'react'
+
+export default function Each<T>({ items, render }: { items?: Array<T>; render(item: T, index: number): ReactNode }) {
+  return items?.map((item, index) => render(item, index))
 }
