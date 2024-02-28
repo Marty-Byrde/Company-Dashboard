9de365b0327a47456fc1cbad63220daa9fb4da73
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-02-28 12:03:41 +0100

    refactor: optimized FeedContainer
    Removed the Fragment that was used to wrap the unordered-list. Now the FeedContainer returns the list itself.

--- app/(components)/Shared/Feeds/FeedContainer.tsx
+++ app/(components)/Shared/Feeds/FeedContainer.tsx
@@ -3,10 +3,8 @@ import { twMerge } from 'tailwind-merge'
 
 export default function FeedContainer({ children, className }: { children?: ReactNode; className?: string }) {
   return (
-    <>
-      <ul role='list' className={twMerge('space-y-6', className)}>
-        {children}
-      </ul>
-    </>
+    <ul role='list' className={twMerge('space-y-6', className)}>
+      {children}
+    </ul>
   )
 }
