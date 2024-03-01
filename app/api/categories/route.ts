ce17bfb0dd60ddb6306fd7fd0847a52dc3acc9eb
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-03-01 11:32:25 +0100

    refactor: exported `dymanic = 'force-dynamic` in routes
    Exported the const dynamic with the value 'force-dynamic'. By doing this the route it self does not cache any results. Which means that whenever the route is called the code within it actually executed. In other words each time the route is opened it will retrieve a set of articles, invoices, categories.
    
    This can be done since each page that uses these endpoints cache the results-anyway.

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,6 +1,7 @@
 import getHellocashAPI from '@/lib/Shared/HelloCash'
 import { NextResponse } from 'next/server'
 
+export const dynamic = 'force-dynamic'
 export async function GET(req: Request) {
   const { getCategories } = getHellocashAPI()
   const categories = await getCategories()
