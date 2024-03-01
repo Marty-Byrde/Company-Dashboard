d99335074dd47e6b925cfc327c867a0969f3b69c
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-03-01 11:34:52 +0100

    refactor: improved code by using hook
    Updated the customer-history page to use the `useBackend` hook to improve the overall code quality.

--- app/customer-history/[cid]/page.tsx
+++ app/customer-history/[cid]/page.tsx
@@ -1,13 +1,19 @@
 import getHellocashAPI from '@/lib/Shared/HelloCash'
 import CustomerBuyHistory from '@/app/customer-history/[cid]/CustomerBuyHistory'
 import { notFound } from 'next/navigation'
+import { Invoice } from 'hellocash-api/typings/Invoice'
+import useBackend from '@/hooks/Shared/Fetch/useBackend'
 
 export default async function CustomerHistoryPage({ params }: { params: { cid?: string } }) {
+  let invoices = await useBackend<Invoice[]>('/invoices?limit=-1', { next: { revalidate: 3600 * 24, tags: ['invoices'] } })
+
   if (!params.cid) return notFound()
   const { findExactCustomer } = getHellocashAPI()
   const customer = await findExactCustomer({ id: params.cid }).then((customers) => customers.at(0))
 
   if (!customer) return <div>Customer not found</div>
 
-  return <CustomerBuyHistory customer={customer} />
+  invoices = invoices.filter((i) => i.customer?.id == customer.id)
+
+  return <CustomerBuyHistory invoices={invoices} />
 }
