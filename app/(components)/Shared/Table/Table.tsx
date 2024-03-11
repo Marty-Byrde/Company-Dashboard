2aaebf39825884b57e8d9a9fe79a578b56e283bd
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-03-11 14:57:53 +0100

    refactor: externalized SelectAll Checkox
    Externalized the select-all checkbox of the table-header. It uses the table-context to access the current selection, items and initial-items.

--- app/(components)/Shared/Table/Table.tsx
+++ app/(components)/Shared/Table/Table.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { createContext, useEffect, useState } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
 import { useDebounce } from 'use-debounce'
 import { twMerge } from 'tailwind-merge'
 import Each from '@/lib/Shared/Each'
@@ -37,7 +37,7 @@ export default function Table<T>({ items: initialItems, visibilities, searchFilt
   }, [debouncedValue])
 
   return (
-    <Context.Provider value={{ labels, visibilities, items: initialItems, selection: selected, searchFilter, isSelected }}>
+    <Context.Provider value={{ labels, visibilities, items, initialItems, selection: selected, setSelection: setSelected, searchFilter, isSelected }}>
       <div className='wrapper relative mt-12 @container 2xs:mt-0'>
         {/*<TableSelectionButtons selection={selected} />*/}
         <div className={twMerge('mb-1 hidden flex-1 items-center justify-end gap-4 @2xs:flex')}>
@@ -54,28 +54,7 @@ export default function Table<T>({ items: initialItems, visibilities, searchFilt
           <thead className='bg-gray-700 py-2 text-left dark:bg-neutral-900'>
             <tr className='space-x-24 text-gray-100 dark:text-gray-200'>
               <th className='relative my-2 block h-8 min-w-16'>
-                <input
-                  type='checkbox'
-                  className='absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded-md border-gray-300 text-indigo-600 hover:cursor-pointer focus:ring-indigo-600 dark:bg-neutral-600 dark:text-fuchsia-700 dark:checked:bg-fuchsia-700 dark:focus:ring-fuchsia-700'
-                  checked={selected.length === initialItems.length || items.map((i) => !!selected.find((s) => s.id === i.id)).every(Boolean)}
-                  onChange={() =>
-                    setSelected((prev) => {
-                      //* The displayed articles are a subset of the initial articles, as a search is active
-                      if (items.length !== initialItems.length) {
-                        //? if all the displayed articles are selected and the total-checkbox is clicked, then the displayed articles are removed from the selection.
-                        if (items.map((i) => !!selected.find((s) => s.id === i.id)).every(Boolean)) {
-                          return prev.filter((a) => !items.find((i) => i.id === a.id))
-                        }
-
-                        //? if not all the displayed articles are selected and the total-checkbox is clicked, then the displayed articles are added to the selection.
-                        return [...prev, ...items.filter((i) => !prev.find((s) => s.id === i.id))]
-                      }
-
-                      //? When all the articles are displayed, thus no search is active, we can simply toggle the selection of all articles.
-                      return prev.length === initialItems.length ? [] : initialItems
-                    })
-                  }
-                />
+                <SelectAllCheckbox />
               </th>
 
               {!labels &&
@@ -178,4 +157,39 @@ function applyFilters<T>(item: TableElement<T>, searchValue: string, filters: Ta
     const key = k as keyof TableElement<T>
     return item[key].toString().toLowerCase().includes(searchValue.toLowerCase())
   })
+}
+
+/**
+ * Renders a checkbox that de- or selects all the items that are currently displayed in the table.
+ */
+function SelectAllCheckbox<T>({ className }: { className?: string }) {
+  const { items, selection, setSelection, initialItems } = useContext<TableContext<T>>(createGenericTableContext())
+
+  return (
+    <input
+      type='checkbox'
+      className={twMerge(
+        'absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded-md border-gray-300 text-indigo-600 hover:cursor-pointer focus:ring-indigo-600 dark:bg-neutral-600 dark:text-fuchsia-700 dark:checked:bg-fuchsia-700 dark:focus:ring-fuchsia-700',
+        className,
+      )}
+      checked={selection.length === initialItems.length || items.map((i) => !!selection.find((s) => s.id === i.id)).every(Boolean)}
+      onChange={() =>
+        setSelection((prev) => {
+          //* The displayed articles are a subset of the initial articles, as a search is active
+          if (items.length !== initialItems.length) {
+            //? if all the displayed articles are selected and the total-checkbox is clicked, then the displayed articles are removed from the selection.
+            if (items.map((i) => !!selection.find((s) => s.id === i.id)).every(Boolean)) {
+              return prev.filter((a) => !items.find((i) => i.id === a.id))
+            }
+
+            //? if not all the displayed articles are selected and the total-checkbox is clicked, then the displayed articles are added to the selection.
+            return [...prev, ...items.filter((i) => !prev.find((s) => s.id === i.id))]
+          }
+
+          //? When all the articles are displayed, thus no search is active, we can simply toggle the selection of all articles.
+          return prev.length === initialItems.length ? [] : initialItems
+        })
+      }
+    />
+  )
 }
