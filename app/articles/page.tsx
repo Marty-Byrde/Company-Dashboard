bc875171acdb3415935bfb7dfbc881d04873710f
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-03-22 17:21:00 +0100

    refactor: replaced `ArticleTable` with the generic `Table`
    Replaced the usage of the `ArticleTable` component with the new generic `Table` component in the /articles page.
    
    Note, that the the category-id for each article has been replaced with the category's name.

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,17 +1,26 @@
-import ArticleTable from '@/app/articles/ArticleTable'
 import { Article } from 'hellocash-api/typings/Article'
 import { ArticleCategory } from 'hellocash-api/typings/Category'
 import useBackend from '@/hooks/Shared/Fetch/useBackend'
+import Table from '@/components/Shared/Table/Table'
 
 export default async function ArticlesPage() {
   const articles = await useBackend<Article[]>('/articles', { next: { revalidate: 3600, tags: ['articles'] } })
   const categories = await useBackend<ArticleCategory[]>('/categories', { next: { revalidate: 3600, tags: ['categories'] } })
 
+  //? This replaces the category id with the category name for each article
+  const shownArticles: Array<Omit<Article, 'category'> & { category?: string }> = articles.map((a) => ({ ...a, category: categories.find((c) => c.id === a.category)?.name }))
+
   return (
     <div className='gap4 flex flex-col'>
       <h1 className='mb-6 text-2xl font-semibold'>Articles</h1>
 
-      <ArticleTable articles={articles} categories={categories} />
+      <Table<(typeof shownArticles)[number]>
+        labels={{ id: 'ID', name: 'Name', category: 'Category' }}
+        visibilities={{ id: 'hidden min-w-16 @xl:table-cell', name: 'w-[100%]', category: 'hidden text-nowrap pr-4 text-right @3xl:table-cell' }}
+        noDefaultLabels
+        items={shownArticles}
+        searchFilter={['name', 'category']}
+      />
     </div>
   )
 }
