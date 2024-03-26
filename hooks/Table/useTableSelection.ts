2ad4318d8250949783979e6e0a4d476116e208f2
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-03-26 15:28:38 +0100

    feat: disabled toggleSelection conditionally
    When the allowSelection property is set to false, then the toggleSelection function, inside the useTableSelection hook, will not change the selection.

--- hooks/Table/useTableSelection.ts
+++ hooks/Table/useTableSelection.ts
@@ -3,10 +3,11 @@
 import { useTableContext } from '@/components/Shared/Table/Table'
 
 export default function useTableSelection<T>() {
-  const { items, selection, setSelection } = useTableContext<T>()
+  const { items, selection, setSelection, allowSelection } = useTableContext<T>()
 
   const isSelected = (item: (typeof items)[number]) => !!selection.find((i) => i.id === item.id)
-  const toggleSelection = (item: (typeof items)[number]) => () => setSelection((prev) => (isSelected(item) ? prev.filter((a) => a.id !== item.id) : [...prev, item]))
+  const toggleSelection = (item: (typeof items)[number]) => () =>
+    allowSelection ? setSelection((prev) => (isSelected(item) ? prev.filter((a) => a.id !== item.id) : [...prev, item])) : null
 
   return { isSelected, toggleSelection }
 }
