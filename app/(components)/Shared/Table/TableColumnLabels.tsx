6e85e1bea5db49dce015cac6d9f3185a3600963d
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-03-26 15:27:35 +0100

    feat: added padding to first-column when selection is disabled
    When the allowSelection property is set to false, the first label receives a padding-left 4.

--- app/(components)/Shared/Table/TableColumnLabels.tsx
+++ app/(components)/Shared/Table/TableColumnLabels.tsx
@@ -10,14 +10,16 @@ import { twMerge } from 'tailwind-merge'
  * @constructor
  */
 export default function TableColumnLabels<T>() {
-  const { labels, visibilities, itemButtons } = useTableContext<T>()
+  const { labels, visibilities, itemButtons, allowSelection } = useTableContext<T>()
   const visiblilty = (key: keyof TableVisibilities<T>) => (visibilities && visibilities.hasOwnProperty(key) ? visibilities[key] : '')
 
   return (
     <>
       <Each
         items={getKeys(labels)}
-        render={(value, index) => <Label key={value.toString() + index} label={labels[value]!} classes={visiblilty(value as keyof TableVisibilities<T>)} />}
+        render={(value, index) => (
+          <Label key={value.toString() + index} label={labels[value]!} classes={`${visiblilty(value as keyof TableVisibilities<T>)} ${index === 0 && !allowSelection && 'pl-4'}`} />
+        )}
       />
 
       {itemButtons && <th className={twMerge('pr-3 text-right', visibilities?.itemButtons)}>Actions</th>}
